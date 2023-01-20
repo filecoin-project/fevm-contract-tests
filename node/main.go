48d378d7651f5618302e5c13391f39ddaf1f2399
ychiao <ychiaoli18@users.noreply.github.com>
2023-01-20 15:20:05 -0500

    fix ci

--- node/main.go
+++ node/main.go
@@ -34,7 +34,6 @@ func initKit() {
 		kit.MockProofs(),
 		kit.ThroughRPC(),
 		kit.WithCfgOpt(func(cfg *config.FullNode) error {
-			cfg.ActorEvent.EnableRealTimeFilterAPI = true
 			cfg.Fevm.EnableEthRPC = true
 			return nil
 		}),
